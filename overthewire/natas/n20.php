292d12cb31134a8df120c0e77830e593b1749c5b
Robert Kovacs <robert.kovacs.centacc@gmail.com>
2019-10-14 21:55:23 +0200

    overthewire natas

--- overthewire/natas/n20.php
+++ overthewire/natas/n20.php
@@ -1,4 +1,4 @@
- <html>
+<html>
 <head>
 <!-- This stuff in the header has nothing to do with the level -->
 <link rel="stylesheet" type="text/css" href="http://natas.labs.overthewire.org/css/level.css">
@@ -11,55 +11,57 @@
 <body>
 <h1>natas20</h1>
 <div id="content">
-<?
+<?php
 
-function debug($msg) { /* {{{ */
-    if(array_key_exists("debug", $_GET)) {
-        print "DEBUG: $msg<br>";
-    }
+function debug($msg) {
+    print "DEBUG: $msg<br>";
 }
-/* }}} */
-function print_credentials() { /* {{{ */
+
+function print_credentials() {
     if($_SESSION and array_key_exists("admin", $_SESSION) and $_SESSION["admin"] == 1) {
-    print "You are an admin. The credentials for the next level are:<br>";
-    print "<pre>Username: natas21\n";
-    print "Password: <censored></pre>";
+        print "You are an admin. The credentials for the next level are:<br>";
+        print "<pre>Username: natas21\n";
+        print "Password: <censored></pre>";
     } else {
-    print "You are logged in as a regular user. Login as an admin to retrieve credentials for natas21.";
+        print "You are logged in as a regular user. Login as an admin to retrieve credentials for natas21.";
     }
 }
-/* }}} */
 
-/* we don't need this */
 function myopen($path, $name) { 
-    //debug("MYOPEN $path $name"); 
     return true; 
 }
 
-/* we don't need this */
 function myclose() { 
-    //debug("MYCLOSE"); 
     return true; 
+}
+
+function mydestroy($sid) {
+    return true;
+}
+function mygarbage($t) {
+    return true;
 }
 
 function myread($sid) { 
-    debug("MYREAD $sid"); 
+    debug("myread - sid: $sid"); 
     if(strspn($sid, "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM-") != strlen($sid)) {
-    debug("Invalid SID"); 
+        debug("myread - Invalid SID"); 
         return "";
     }
     $filename = session_save_path() . "/" . "mysess_" . $sid;
     if(!file_exists($filename)) {
-        debug("Session file doesn't exist");
+        debug("myread - Session file doesn't exist");
         return "";
     }
-    debug("Reading from ". $filename);
+    debug("myread - Reading from ". $filename);
     $data = file_get_contents($filename);
     $_SESSION = array();
     foreach(explode("\n", $data) as $line) {
-        debug("Read [$line]");
-    $parts = explode(" ", $line, 2);
-    if($parts[0] != "") $_SESSION[$parts[0]] = $parts[1];
+        debug("myread - line [$line]");
+        $parts = explode(" ", $line, 2);
+        if($parts[0] != "") {
+            $_SESSION[$parts[0]] = $parts[1];
+        }
     }
     return session_encode();
 }
@@ -67,33 +69,24 @@ function myread($sid) {
 function mywrite($sid, $data) { 
     // $data contains the serialized version of $_SESSION
     // but our encoding is better
-    debug("MYWRITE $sid $data"); 
+    debug("mywrite - $sid $data"); 
     // make sure the sid is alnum only!!
     if(strspn($sid, "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM-") != strlen($sid)) {
-    debug("Invalid SID"); 
+        debug("mywrite - Invalid SID"); 
         return;
     }
     $filename = session_save_path() . "/" . "mysess_" . $sid;
     $data = "";
-    debug("Saving in ". $filename);
+    debug("mywrite - Saving in ". $filename);
     ksort($_SESSION);
     foreach($_SESSION as $key => $value) {
-        debug("$key => $value");
+        debug("mywrite - $key => $value");
         $data .= "$key $value\n";
     }
+    debug("mywrite - data: $data");
     file_put_contents($filename, $data);
     chmod($filename, 0600);
-}
-
-/* we don't need this */
-function mydestroy($sid) {
-    //debug("MYDESTROY $sid"); 
-    return true; 
-}
-/* we don't need this */
-function mygarbage($t) { 
-    //debug("MYGARBAGE $t"); 
-    return true; 
+    return true;
 }
 
 session_set_save_handler(
@@ -119,11 +112,10 @@ if(array_key_exists("name", $_SESSION)) {
 
 ?>
 
-<form action="index.php" method="POST">
+<form action="natas20.php" method="POST">
 Your name: <input name="name" value="<?=$name?>"><br>
 <input type="submit" value="Change name" />
 </form>
-<div id="viewsource"><a href="index-source.html">View sourcecode</a></div>
 </div>
 </body>
-</html> 
+</html>
